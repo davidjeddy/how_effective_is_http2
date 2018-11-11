72846402bfc251d88abf5e5ffb441140bf56b9e0
davidjeddy <me@davidjeddy.com>
2018-11-11 13:35:43 -0500

    Cleaned up wordpress_install.sh a bit.

--- configs/shared/wordpress_install.sh
+++ configs/shared/wordpress_install.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 # install wordpress files
- curl -LO https://wordpress.org/latest.tar.gz
- tar xzvf latest.tar.gz
- cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
- rm -rf /var/www/html
- mv ./wordpress /var/www/html
-
- # Change www properties
- chmod -R 0755 /var/www/html
- chown -R www-data:www-data /var/www/html
+curl -LO https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
+rm -rf /var/www/html
+mv ./wordpress /var/www/html
 
 # Place current IP in dump.SQL
-CURRENT_IP="test.davidjeddy.com"
-sed -i "s/CURRENT_IP/$CURRENT_IP/g" /root/dump.sql
+sed -i "s/CURRENT_IP/test\.davidjeddy\.com/g" /root/dump.sql
 
 # setup MySQL database user
 sudo mysql -uroot -proot -hlocalhost -e "DROP DATABASE IF EXISTS database_name_here; CREATE DATABASE database_name_here;  GRANT ALL PRIVILEGES ON *.* TO 'username_here'@'localhost' IDENTIFIED BY '2!3swZ$NVO(wcigcw('; FLUSH PRIVILEGES;"
@@ -24,7 +19,8 @@ mysql -uroot -proot database_name_here < /root/dump.sql
 # unzip content images into WP root
 unzip /root/wp-content.zip /var/www/html
 
-# change perms
+# Change www properties
+chmod -R 0755 /var/www/html
 chown -R www-data:www-data /var/www/html
 
 # restart services to take new configurations
